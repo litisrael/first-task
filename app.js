5179987640ff63fc79480cb01f4f0e018369384f
israel lit <isroellit@gmail.com>
2022-12-14 00:39:12 +0200

    first task app file of inform

---
+++ app.js
@@ -0,0 +1,42 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { appendFile } from 'node:fs'
+
+
+
+const rl = readline.createInterface({ input, output });
+    console.log("welcome to weight test!!!\n")
+
+    const name = await rl.question("write your first and last name\n",(answer) => {
+
+    })
+
+    const gender =  await rl.question('gender: answer "M" or "F"\n', (answer) => {
+
+    })
+
+    const kilos = await rl.question("weight in kilos!!\n", (answer) => {
+
+    })
+
+    const meter = await rl.question("height in meters!\n", (answer) => {
+
+    })
+
+    rl.close();
+    const users= []
+
+    let user = {
+        name: name,
+        gender: gender,
+        kilos: kilos,
+        meter:meter,
+    
+    } 
+   users.push(user)
+    
+    appendFile("./users.jsob", JSON.stringify(users),(err)=>{
+        if(err){
+            console.log(err)
+            exit(1) 
+        }})
