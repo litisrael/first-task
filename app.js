8bbecf8bc785d23ac7a3786d191ab68c273472c6
israel lit <isroellit@gmail.com>
2022-12-15 17:12:00 +0200

    modific name

--- app.js
+++ app.js
@@ -7,17 +7,21 @@ import { appendFile } from 'node:fs'
 const rl = readline.createInterface({ input, output });
     console.log("welcome to weight test!!!\n")
 
-    const name = await rl.question("write your first and last name\n",(answer) => {
-        if (name.length< 3){return"name should have more than 4 letters"}
+    const askForName = async (msg = null) => {
+      if (msg) console.log(msg)
+      const answer = await rl.question("Write your first and last name\n")
+      return answer.length < 4 ? askForName("name should have more than 4 letters") : answer
+    }
 
-    })
-    let gender
+    let name = await askForName()
+    
+    let gender= null
     while (gender !== 'M' && gender !== 'F') {
         gender = await rl.question('gender: Please answer "M" or "F":\n');
       }
       
 
-      let kilos = 0;
+      let kilos = null;
 
       while (isNaN(kilos) || kilos < 10) {
         kilos = await rl.question("add the weight in kilos (must be greater than or equal to 10):\n");
@@ -40,12 +44,12 @@ const rl = readline.createInterface({ input, output });
         name: name,
         gender: gender,
         kilos: kilos,
-        meter:meter ,
+        meter:meter 
     
     } 
-   
+  
     
-    appendFile("./user.json", JSON.stringify(user) + "\n",(err)=>{
+    appendFile("./user.json", JSON.stringify(user) +","+ "\n",(err)=>{
         if(err){
             console.log(err)
             exit(1) 
