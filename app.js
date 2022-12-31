2c6f06f0af5f0e504af176d299f0a4c96716a3ac
israel lit <isroellit@gmail.com>
2022-12-31 19:29:08 +0200

    list of map work

--- app.js
+++ app.js
@@ -2,21 +2,32 @@
 import { appendFile, readFile, writeFile,readFileSync } from 'node:fs'
 import { rl, askForName,askForGender,askForHeight,askForWeight } from './question.js';
 import { lastNumber ,nextNumber } from './control.js';
+export const usersMap = new Map()
+const asignarId =()=> Math.random().toString(36).substr(2, 9);
+    let id = asignarId()
+ const  addToMap =(id) => usersMap.set(id,usersMap.size);
+ const mapActualy= addToMap()
 
 const addEmptyChars = (str, targetLength=99) => {
   const countLong = targetLength - str.length 
   const completeTo35 = (num) => ' '.repeat(num)
-
   return str + completeTo35(countLong ) 
 }
 
-export const usersMap = new Map()
-let currentValue = 0;
-
-function addToMap() {
-  usersMap.set(usersMap.size, currentValue + 1);
-  currentValue++;
+const  listMap  = async () => {
+  await appendFile("./useresMap.txt",JSON.stringify(mapActualy) + "\n" ,(err)=>{
+    if(err){
+        console.log(err)
+        exit(1) 
+    }
+  })
 }
+
+
+
+
+  
+
 
 // const updateUsersMap = async (id, lastNumber) => {
 //   const data = await readFile('./usersMap.json', 'utf8', (err, data) => {
@@ -34,14 +45,7 @@ function addToMap() {
 //     }) 
 // }
 
-const listMap = () =>{
-  appendFile("./useresMap.txt",addToMap(user.id) ,(err)=>{
-    if(err){
-        console.log(err)
-        exit(1) 
-    }
-  })
-}
+
 const createUser = (user) => {
   appendFile("./users.txt", addEmptyChars(JSON.stringify(user)) + "\n",(err)=>{
     if(err){
@@ -51,30 +55,31 @@ const createUser = (user) => {
   })
    addToMap(user.id);
    listMap();
+   
 }
 
 
-     const name = await askForName()
-     const gender= await askForGender()
-     const kilos = await askForWeight()
+
+    const name = await askForName()
+    const gender= await askForGender()
+    const kilos = await askForWeight()
     const meter = await askForHeight()
 
     rl.close();
     
     
-    const asignarId =()=> Math.random().toString(36).substr(2, 9);
     
       createUser({
-          id: asignarId() ,
-          name:name ,
-          gender:gender,
-          kilos:kilos ,
-          meter:meter
+        id: id ,
+        name:name ,
+        gender:gender,
+         kilos:kilos ,
+         meter:meter
         })
 
-      //console.log(`thank you ${name}, your data is complete`)
+      console.log(`thank you ${name}, your data is complete`)
 
-    console.log(usersMap.get('z36cgutzx'))
+    console.log(...usersMap.entries())
    
 
        
